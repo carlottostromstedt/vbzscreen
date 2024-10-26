ab611eeb8726e6ade17b483f952d086c7ea66cda
Carl Otto Strömstedt <carl.ottostromstedt@gmail.com>
2024-10-26 16:04:35 +0200

    fix: code 2

--- scripts/display_connections.py
+++ scripts/display_connections.py
@@ -132,72 +132,91 @@ def get_weather():
 logging.basicConfig(level=logging.INFO)  # Configure logging level
 
 def fetch_and_display_connections(epd, draw, counter, weather_counter, temperature, weather_description, temperature_max, temperature_min):
-    should_sleep = False
-    amount_to_sleep = 0
-    try:
-        logging.info("Fetching connections from API...")
-        response = requests.get(URL)
-        response.raise_for_status()  # Raise an exception for non-200 status codes
-        data = json.loads(response.text)
+  should_sleep = False
+  amount_to_sleep = 0
+  try:
+    logging.info("Fetching connections from API...")
+    response = requests.get(URL)
+    response.raise_for_status()  # Raise an exception for non-200 status codes
+    data = json.loads(response.text)
 
-        connections = data["stationboard"]
-        connections_sorted = sorted(connections, key=lambda x: get_departure_time(x))  
+    connections = data["stationboard"]
+    connections_sorted = sorted(connections, key=lambda x: get_departure_time(x))  
 
-        x = 10
-        y = 12
-        amount_displayed = 0
+    x = 10
+    y = 12
+    amount_displayed = 0
 
-        # Create an empty image for text drawing
-        text_image = Image.new('1', (image.height, image.width), 255)  # White background
-        text_draw = ImageDraw.Draw(text_image)
+    # Create an empty image for text drawing
+    text_image = Image.new('1', (image.height, image.width), 255)  # White background
+    text_draw = ImageDraw.Draw(text_image)
 
-        for connection in connections_sorted:
-            # Drawing logic remains the same...
-            # Draw text on the blank image
-            if int(minutes_to_departure) > 3 and amount_displayed < 5:
-                # Draw the text in the appropriate location
-                text_draw.text((x, y), f"{number}", font=font, fill=0)
-                text_draw.text(((x + 15), y), f"{destination}", font=font, fill=0)
-                text_draw.text((200, y), f"{minutes_to_departure_string}", font=font, fill=0)
-                y = y + 24
-                amount_displayed += 1
+    # Iterate over connections to draw text on the blank image
+    for connection in connections_sorted:
+        number = connection["number"]
+        departure = connection["stop"]["prognosis"]["departure"]
+        destination = connection["to"]
+        destination = remove_zurich(destination)
 
-            if int(minutes_to_departure) > 40 and amount_displayed < 5:
-                should_sleep = True
-                amount_to_sleep = (int(minutes_to_departure) - 1) * 60
+        if number == None:
+            number = "N"
 
-        current_hour = datetime.now().strftime("%H") 
-        current_minutes = datetime.now().strftime("%M")
-        text_draw.text((220, 12), f"{current_hour}:{str(current_minutes)}", font=font_time, fill=0)
-
-        # Draw weather information
-        if counter == 5 or weather_counter == 0:
-            temperature, weather_description, temperature_min, temperature_max = get_weather() 
-            weather_counter += 1
-
-        text_draw.text((220, 55), f"{int(temperature)}°C" , font=font_weather, fill=0)
-        text_draw.text((220, 82), f"{weather_description}" , font=font_weather, fill=0)
-        text_draw.text((220, 107), f"H: {temperature_max}°C L: {int(temperature_min)}°C", font=font_weather2, fill=0)
-
-        # Rotate the text image by 180 degrees if the --rotate flag is set
-        if args.rotate:
-            rotated_text_image = text_image.rotate(180, expand=True)  # Rotate by 180 degrees
+        if departure != None:
+            minutes_to_departure = departure_to_minutes(departure)
         else:
-            rotated_text_image = text_image.rotate(90, expand=True)  # Original 90-degree rotation
+            minutes_to_departure = departure_to_minutes(connection["stop"]["departure"])
 
-        image.paste(rotated_text_image)
+        # Draw text on the blank image
+        if int(minutes_to_departure) > 3 and amount_displayed < 5:
+          # Draw the text in the appropriate location
+          text_draw.text((x, y), f"{number}", font=font, fill=0)
+          text_draw.text(((x + 15), y), f"{destination}", font=font, fill=0)
+          text_draw.text((200, y), f"{minutes_to_departure_string}", font=font, fill=0)
+          y = y + 24
+          amount_displayed += 1
 
-        # Display on screen
-        if counter > 0:
-            epd.display_Partial_Wait(epd.getbuffer(image))
-        else:
-            epd.display_Base(epd.getbuffer(image))
+        if int(minutes_to_departure) > 40 and amount_displayed < 5:
+            should_sleep = True
+            amount_to_sleep = (int(minutes_to_departure) - 1) * 60
 
-    except requests.exceptions.RequestException as err:
-        logging.error("Error fetching data:", err)
+    current_hour = datetime.now().strftime("%H") 
+    current_minutes = datetime.now().strftime("%M")
 
-    return should_sleep, amount_to_sleep, weather_counter, temperature, weather_description, temperature_max, temperature_min
+    text_draw.text((220, 12), f"{current_hour}:{str(current_minutes)}", font=font_time, fill=0)
 
+    if counter == 5 or weather_counter == 0:
+        temperature, weather_description, temperature_min, temperature_max = get_weather() 
+        weather_counter += 1
+        logging.info(f"Weather counter: {weather_counter}")
+
+    text_draw.text((220, 55), f"{int(temperature)}°C" , font=font_weather, fill=0)
+    text_draw.text((220, 82), f"{weather_description}" , font=font_weather, fill=0)
+    text_draw.text((220, 107), f"H: {temperature_max}°C L: {int(temperature_min)}°C", font=font_weather2, fill=0)
+
+    # Rotate the text image and paste it onto the main image
+    rotated_text_image = text_image.rotate(90, expand=True)  # Rotate and expand
+     # Rotate the text image by 180 degrees if the --rotate flag is set
+    if args.rotate:
+        rotated_text_image = text_image.rotate(180, expand=True)  # Rotate by 180 degrees
+    else:
+        rotated_text_image = text_image.rotate(90, expand=True)  # Original 90-degree rotation
+
+    image.paste(rotated_text_image)
+
+    if counter > 0:
+        epd.display_Partial_Wait(epd.getbuffer(image))
+        logging.info("Displaying partial")
+    else:
+        epd.display_Base(epd.getbuffer(image))
+        logging.info("Displaying Base")# Update screen
+
+    logging.info("All lines displayed simultaneously")
+     
+  except requests.exceptions.RequestException as err:
+    logging.error("Error fetching data:", err)
+
+  logging.info("Waiting for next update...")
+  return should_sleep, amount_to_sleep, weather_counter, temperature, weather_description, temperature_max, temperature_min
 
 epd = epd2in9_V2.EPD_2IN9_V2()
 tp = icnt86.INCT86()
